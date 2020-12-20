578c0c7436aa94f011caf596d64144642a240c6a
Dalton Mills <mills.dalton@gmail.com>
2020-12-19 23:22:51 -0600

    Add greatsword math

--- src/data/weapons/WpnTypeGreatsword.js
+++ src/data/weapons/WpnTypeGreatsword.js
@@ -1,3 +1,5 @@
+import { game } from '../../core/Game';
+import { getAdjacentDeltas, directionFromDelta } from '../../enums/Directions';
 import Attack from '../Attack';
 import { DMG_TYPE_SLASHING } from '../DamageTypes';
 import { getStat, STAT_STRENGTH } from '../Stats';
@@ -20,14 +22,38 @@ export default class WpnTypeGreatsword extends WeaponType {
         const die = weapon.roll();
         const damage = die + str;
 
+        const attackerPos = attacker.position.getPos();
+        const defenderPos = defender.position.getPos();
+        const deltaX = defenderPos.x - attackerPos.x;
+        const deltaY = defenderPos.y - attackerPos.y;
+        const direction = directionFromDelta(deltaX, deltaY);
+        const adjacentDeltas = getAdjacentDeltas(direction.key);
+        const adjacentAttacks = adjacentDeltas
+            .map((delta) => game.map.getEntitiesAt(
+                attackerPos.x + delta.x,
+                attackerPos.y + delta.y,
+            ))
+            .flat()
+            .filter((e) => game.factions.areEntitiesHostile(e, attacker))
+            .map((hostile) => {
+                return new Attack({
+                    attacker,
+                    defender: hostile,
+                    weaponName: weapon.name,
+                    damage: Math.floor(damage / 2),
+                    damageType: this.damageType,
+                });
+            });
+
         return [
+            ...adjacentAttacks,
             new Attack({
                 attacker,
                 defender,
                 weaponName: weapon.name,
                 damage,
                 damageType: this.damageType,
-            })
+            }),
         ];
     }
 }
--- src/enums/Directions.js
+++ src/enums/Directions.js
@@ -43,6 +43,7 @@ const directions = [
             x: 0,
             y: 0,
         },
+        adjacent: [],
     },
     {
         key: 5,
@@ -92,8 +93,27 @@ export const DIR_SW = 6;
 export const DIR_S = 7;
 export const DIR_SE = 8;
 
+directions[DIR_NW].adjacent = [DIR_N, DIR_W];
+directions[DIR_N].adjacent = [DIR_NW, DIR_NE];
+directions[DIR_NE].adjacent = [DIR_N, DIR_E];
+directions[DIR_W].adjacent = [DIR_NW, DIR_SW];
+directions[DIR_E].adjacent = [DIR_NE, DIR_SE];
+directions[DIR_SW].adjacent = [DIR_W, DIR_S];
+directions[DIR_S].adjacent = [DIR_SW, DIR_SE];
+directions[DIR_SE].adjacent = [DIR_S, DIR_E];
+
 export const shortName = (direction) => directions[direction].shortName;
 
 export const fullName = (direction) => directions[direction].fullName;
 
 export const delta = (direction) => directions[direction].delta;
+
+export const adjacent = (direction) => directions[direction].adjacent;
+
+export const getAdjacentDeltas = (direction) => adjacent(direction).map(delta);
+
+export const directionFromDelta = (x, y) => {
+    return Object.values(directions).find((dir) => {
+        return dir.delta.x === x && dir.delta.y === y;
+    });
+};
